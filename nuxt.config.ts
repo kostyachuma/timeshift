ece7210cb8283603d58ca27c60dd3432ee198b74
Kostya Chuma <constantinechumachenko@gmail.com>
2023-01-31 17:03:42 +0100

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    routeRules: {
-        '/': { ssr: false },
-    },
+    ssr: false,
     modules: ['@nuxtjs/tailwindcss']
 })
