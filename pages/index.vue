cdb39f188ab6348ebc24d0efbdcf5701e946be28
Kostya Chuma <constantinechumachenko@gmail.com>
2023-04-17 16:58:12 +0200

    fix zones list

--- pages/index.vue
+++ pages/index.vue
@@ -132,67 +132,67 @@
       "name": "Azores, Cape Verde Islands"
     },
     {
-      "utcOffsetStr": "00:00",
+      "utcOffsetStr": "+00:00",
       "dstOffsetStr": "+01:00",
       "name": "Western Europe Time, London, Lisbon, Casablanca"
     },
     {
-      "utcOffsetStr": "01:00",
+      "utcOffsetStr": "+01:00",
       "dstOffsetStr": "+02:00",
       "name": "Brussels, Copenhagen, Madrid, Paris"
     },
     {
-      "utcOffsetStr": "02:00",
+      "utcOffsetStr": "+02:00",
       "dstOffsetStr": "+03:00",
       "name": "Kaliningrad, South Africa"
     },
     {
-      "utcOffsetStr": "03:00",
+      "utcOffsetStr": "+03:00",
       "dstOffsetStr": "+04:00",
       "name": "Baghdad, Riyadh, Moscow, St. Petersburg"
     },
     {
-      "utcOffsetStr": "04:00",
+      "utcOffsetStr": "+04:00",
       "dstOffsetStr": "+04:00",
       "name": "Abu Dhabi, Muscat, Baku, Tbilisi"
     },
     {
-      "utcOffsetStr": "05:00",
+      "utcOffsetStr": "+05:00",
       "dstOffsetStr": "+05:00",
       "name": "Ekaterinburg, Islamabad, Karachi, Tashkent"
     },
     {
-      "utcOffsetStr": "06:00",
+      "utcOffsetStr": "+06:00",
       "dstOffsetStr": "+06:00",
       "name": "Almaty, Dhaka, Colombo"
     },
     {
-      "utcOffsetStr": "07:00",
+      "utcOffsetStr": "+07:00",
       "dstOffsetStr": "+07:00",
       "name": "Bangkok, Hanoi, Jakarta"
     },
     {
-      "utcOffsetStr": "08:00",
+      "utcOffsetStr": "+08:00",
       "dstOffsetStr": "+08:00",
       "name": "Beijing, Perth, Singapore, Hong Kong"
     },
     {
-      "utcOffsetStr": "09:00",
+      "utcOffsetStr": "+09:00",
       "dstOffsetStr": "+09:00",
       "name": "Tokyo, Seoul, Osaka, Sapporo, Yakutsk"
     },
     {
-      "utcOffsetStr": "10:00",
+      "utcOffsetStr": "+10:00",
       "dstOffsetStr": "+10:00",
       "name": "Eastern Australia, Guam, Vladivostok"
     },
     {
-      "utcOffsetStr": "11:00",
+      "utcOffsetStr": "+11:00",
       "dstOffsetStr": "+11:00",
       "name": "Magadan, Solomon Islands, New Caledonia"
     },
     {
-      "utcOffsetStr": "12:00",
+      "utcOffsetStr": "+12:00",
       "dstOffsetStr": "+12:00",
       "name": "Auckland, Wellington, Fiji, Kamchatka"
     }
@@ -225,6 +225,7 @@
     const groupByOffset = _.groupBy(selectedZones, 'utcOffsetStr');
 
     const formatedGroups = Object.entries(groupByOffset).map(([key, zones]) => {
+      console.log(key)
       const { name, utcOffsetStr, dstOffsetStr } = timeZonesNamesByOffset.find(
         ({ utcOffsetStr }) => utcOffsetStr === key
       );
