7f4d117d9ff8bf970e3c3c0f3702f4bb3041c905
Kostya Chuma <constantinechumachenko@gmail.com>
2025-02-19 14:40:58 +0100

    remove output

--- .output/public/_nuxt/BDBqv_EU.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,v as n,t as r}from"./JidMSw1s.js";const c={};function t(a,o){return r(),n("div",null,"Planner")}const _=e(c,[["render",t]]);export{_ as default};
--- .output/public/_nuxt/D7NtkjJX.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,v as t,t as c}from"./JidMSw1s.js";const n={};function r(s,o){return c(),t("div",null,"Cities")}const a=e(n,[["render",r]]);export{a as default};
